37e4b33c7dabc8d27cbc3aa0b249f9ea41f29d21
= <=>
2019-01-15 10:50:12 +0100

    Creating pages

---
+++ create.php
@@ -0,0 +1,108 @@
+
+<!DOCTYPE html>
+
+<html lang="fr">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
+        crossorigin="anonymous">
+    <link rel="stylesheet" href="../assets/css/main.css">
+
+
+    <title>Films</title>
+
+</head>
+
+<body>
+
+<form>
+
+  <div class="form-group">
+
+    <label for="exampleFormControlInput1">Title</label>
+    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+
+  </div>
+
+  <div class="form-group">
+
+    <label for="exampleFormControlSelect1">Genre</label>
+    <select class="form-control" id="exampleFormControlSelect1">
+
+      <option>Horror</option>
+      <option>Comedy</option>
+      <option>Romance</option>
+      <option>Action</option>
+
+    </select>
+
+  </div>
+
+  <div class="form-group">
+
+    <label for="exampleFormControlTextarea1">Film Length</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+
+  <div class="form-group">
+
+    <label for="exampleFormControlTextarea1">Release Date</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+
+  <div class="form-group">
+
+    <label for="exampleFormControlTextarea1">Director</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+
+  <div class="form-group">
+
+    <label for="exampleFormControlTextarea1">Main ACtor</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+
+  <div class="form-group">
+
+    <label for="exampleFormControlSelect2">Rating</label>
+    <select multiple class="form-control" id="exampleFormControlSelect2">
+
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+
+    </select>
+
+  </div>
+
+  <div class="form-group">
+
+    <label for="exampleFormControlTextarea1">Example textarea</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+</form>
+
+
+
+
+
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+<script src="../assets/js/main.js"></script>
+    
+</body>
+</html>
+
+
